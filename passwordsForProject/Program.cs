1189352cca0870aac819fb10707f535055e16e39
PrincetonE123 <149517412+PrincetonE123@users.noreply.github.com>
2024-12-18 13:40:57 -0600

    Add files via upload

---
+++ passwordsForProject/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Diagnostics;
+using System.Security.Cryptography;
+using System.Text;
+using BCrypt.Net;
+
+class Program
+{
+    static void Main()
+    {
+        string myPassword = "fastcars";
+        //string saltedPassword = myPassword + randomString;
+        string hashedPass = BCrypt.Net.BCrypt.HashPassword(myPassword,13);
+
+        Console.WriteLine(hashedPass);
+
+    }
+
+} 
